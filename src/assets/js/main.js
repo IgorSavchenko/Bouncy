0b151f1487d5f32377f62f62d69e9d6d07e28331
IgorSavchenko <ig0r@ua.fm>
2017-09-16 10:57:07 +0300

    Initial commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,30 @@
+//----------------------------------------------------
+// init siema slider
+// import Siema from 'siema';
+//
+// function onInitCallback() {
+//   console.log('Siema initialised bro :)');
+// }
+//
+// function onChangeCallback() {
+//   console.log(`The index of current slide is: ${this.currentSlide}`);
+// }
+//
+// const mySiema = new Siema({
+//   onInit: onInitCallback,
+//   onChange: onChangeCallback,
+// });
+//----------------------------------------------------
+// init Masonry with element when all images are Loaded
+// import Masonry from 'masonry-layout';
+// import imagesLoaded from 'imagesloaded';
+// let imgLoad = imagesLoaded('.grid');
+// imgLoad.on( 'done', function(instance, image) {
+//   //creates Masonry object from .grid
+//   let grid = document.querySelector('.grid');
+//   let msnry = new Masonry( grid, {
+//     // options
+//     itemSelector: ".grid-item"
+//     // columnWidth: 25%
+//   });
+// });
